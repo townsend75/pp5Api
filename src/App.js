ae2840e26347023b0f0fbd1c8df7633b0b20da96
townsend75 Townsend <dtownsend@gmx.net>
2023-11-27 10:13:14 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Maybe this is the start of something crazy</h1>
+    </div>
+  );
+}
+
+export default App;
